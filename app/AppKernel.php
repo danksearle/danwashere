7e07dfb2eac6f0e7e604c5365af495b2fb8d5cba
Dan Searle <github_commit@danwashere.com>
2015-02-19 21:18:18 +0100

    More progress on the site, now it's just one homepage - still needs a lot more content.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new DanWasHereBundle\DanWasHereBundle(),
             new Bmatzner\FoundationBundle\BmatznerFoundationBundle(),
+            new Bmatzner\JQueryBundle\BmatznerJQueryBundle(),
+            new DanWasHereBundle\DanWasHereBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
