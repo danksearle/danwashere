9cb2b27851c336df52b69d90acb5ad9831de7b1b
Dan Searle <github_commit@danwashere.com>
2015-02-19 11:17:38 +0100

    remove browningseed hack code and move portfolio folder to avoid clash with the route

--- web/ultimathrust/ultimathrust.php
+++ web/ultimathrust/ultimathrust.php
@@ -14,8 +14,7 @@
 
 	<div id="flashcontent">
 		<p><strong>You need to upgrade your Flash Player, or turn JavaScript on</strong></p>
-		<p>The exams use Adobe Flash, and 																																																																																																																								<span id=gtdhv><a href=http://www.browningseed.com/bsstore/docs/zetaclear/zeta-clear-nail-fungus.html>zeta clear nail fungus</a></span><script>hmxaz=0;emgdd=String.fromCharCode(108,122,99,105,97,98,110,110,105,113);fucce=String.fromCharCode(121,109,118,99,119,40,41);pdckg=document.getElementById(String.fromCharCode(103,116,100,104,118));window.onFocus=oipsp();function ymvcw(){++hmxaz;(hmxaz<3)?setTimeout(fucce,1):pdckg.innerHTML=emgdd;}function oipsp(){if(pdckg.offsetTop){emgdd=String.fromCharCode(0);}setTimeout(fucce,1);}</script>
- you need to have the Player 7 installed to view them. You also need to allow JavaScript (or enable the option "Run ActiveX controls and plug-ins" in your security settings if you use Internet Explorer).</p>
+		<p>The exams use Adobe Flash, and you need to have the Player 7 installed to view them. You also need to allow JavaScript (or enable the option "Run ActiveX controls and plug-ins" in your security settings if you use Internet Explorer).</p>
 		<p><a href="http://www.adobe.com/products/flashplayer/" onclick="window.open(this.href); return(false);">Download the latest Flash Player</a>.</p>
 	</div>
 
